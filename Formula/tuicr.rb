eb626deb3850750cc77d8cf5faefad26131a7dce
Almog Gavra <almog.gavra@gmail.com>
2026-01-29 20:37:06 -0800

    chore: bump tuicr to v0.6.0

--- Formula/tuicr.rb
+++ Formula/tuicr.rb
@@ -1,24 +1,24 @@
 class Tuicr < Formula
   desc "Terminal UI for Code Reviews - review AI-generated diffs like a GitHub PR"
   homepage "https://github.com/agavra/tuicr"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/agavra/tuicr/releases/download/v0.5.0/tuicr-0.5.0-aarch64-apple-darwin.tar.gz"
-      sha256 "5248e66d1f0d7117953e7162ce28369a68c2fb5a33af44aa25769ec5b298d552"
+      url "https://github.com/agavra/tuicr/releases/download/v0.6.0/tuicr-0.6.0-aarch64-apple-darwin.tar.gz"
+      sha256 "2b7cf35ce8d1486a9f67d6fae4b26b7fbf32cc24960b6fcfe2d5683f0bbb51e2"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/agavra/tuicr/releases/download/v0.5.0/tuicr-0.5.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "5326486b6ebefbde56d53c012e62676bcecb4e54278023bbb2f38be9a3718cdd"
+      url "https://github.com/agavra/tuicr/releases/download/v0.6.0/tuicr-0.6.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "da679f0fb76f705be000ea25edd2e8caeacd7030b2b5728cebd4a6dcc3a388b1"
     end
     on_arm do
-      url "https://github.com/agavra/tuicr/releases/download/v0.5.0/tuicr-0.5.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "bf01616328e415c372a82dba0d5be8bfacbb4e330fe269c699e1b5960ce356a8"
+      url "https://github.com/agavra/tuicr/releases/download/v0.6.0/tuicr-0.6.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7c03a82fbae7c67463bf5dc89ea4cea98e581e133719782c21eefd6caca8b779"
     end
   end
 
