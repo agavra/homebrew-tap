fddc928f4a7f90ba02a67a6239698f7ec80447c3
Almog Gavra <almog.gavra@gmail.com>
2026-07-13 16:00:52 -0700

    tuicr: update to v0.19.1

--- Formula/tuicr.rb
+++ Formula/tuicr.rb
@@ -1,28 +1,28 @@
 class Tuicr < Formula
   desc "Terminal UI for Code Reviews - review AI-generated diffs like a GitHub PR"
   homepage "https://github.com/agavra/tuicr"
-  version "0.19.0"
+  version "0.19.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/agavra/tuicr/releases/download/v0.19.0/tuicr-0.19.0-x86_64-apple-darwin.tar.gz"
-      sha256 "55367984fa6bf5f487bf77409b723138a5bc460e3b55b887aac0e5928db61ad6"
+      url "https://github.com/agavra/tuicr/releases/download/v0.19.1/tuicr-0.19.1-x86_64-apple-darwin.tar.gz"
+      sha256 "46cb4732c98839220b5d5b8dd099450dd35b4de24dcf873ef47b0ad944519f08"
     end
     on_arm do
-      url "https://github.com/agavra/tuicr/releases/download/v0.19.0/tuicr-0.19.0-aarch64-apple-darwin.tar.gz"
-      sha256 "f276027163d139eff70e7ad2626405c578547ac8053ac12d1bf75d92a09b03f9"
+      url "https://github.com/agavra/tuicr/releases/download/v0.19.1/tuicr-0.19.1-aarch64-apple-darwin.tar.gz"
+      sha256 "f89a6682defb9b81084ee89dde6bde64b7fe6e8b90788a7a3490d2d6cacbf2d4"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/agavra/tuicr/releases/download/v0.19.0/tuicr-0.19.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "41e62aabc379838c1b30be95fa0b14b0061821e9188886c9a33516b778f7c815"
+      url "https://github.com/agavra/tuicr/releases/download/v0.19.1/tuicr-0.19.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "67addacc28ee9c6d1ce84220954f1ff0c6a82b374822142dd8cb615b3d425ec5"
     end
     on_arm do
-      url "https://github.com/agavra/tuicr/releases/download/v0.19.0/tuicr-0.19.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "3ca8e99e82e49fbdcab9d69725bbd56f4a8773a0225abbebda9f11fd6104a164"
+      url "https://github.com/agavra/tuicr/releases/download/v0.19.1/tuicr-0.19.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "070c5cee0862c51cfb9718591c7faa4c163a44153cb9b596d107cfb5c86c99ab"
     end
   end
 
